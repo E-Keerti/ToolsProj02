c39b6803755b200a859b69bfdcc51c7e88083a8b
dell <dell@DESKTOP-59V4N07>
2024-11-27 15:56:07 +0530

    commit

--- Maven-proj01/src/test/java/com/nit/main/AppTest.java
+++ Maven-proj01/src/test/java/com/nit/main/AppTest.java
@@ -1,7 +1,14 @@
  package com.nit.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest 
 {
-    
+	public void testsumwithpositive()
+	{
+      ArithmeticOperation app =new ArithmeticOperation();
+      int actual = app.sum(10,20);
+      int execepted =30;
+      assertEquals(execepted,actual);
+	}
 }
