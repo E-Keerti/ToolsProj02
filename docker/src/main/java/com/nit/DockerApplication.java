03dc9ddb2a062a0de53a13bf0ecb1b77bee45436
dell <dell@host.docker.internal>
2024-11-30 11:02:52 +0530

    SpringBoot App Commit

---
+++ docker/src/main/java/com/nit/DockerApplication.java
@@ -0,0 +1,15 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DockerApplication.class, args);
+		System.out.println("Welcome to Docker Apps");
+	}
+
+}
